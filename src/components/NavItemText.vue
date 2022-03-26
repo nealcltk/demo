a7d00a8316d1400323ebf54a59efde4c47201055
Neal Cynkus <neal.cynkus@rewardstyle.com>
2022-03-25 22:07:26 -0700

    suspense

---
+++ src/components/NavItemText.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="nav-item" id="nav-item">
+    <router-link v-if="isInternalLink" :to="link.href">{{
+      link.label
+    }}</router-link>
+    <a v-else :href="link.href">{{ link.label }}</a>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+interface INavItemProps {
+  label: string;
+  href: string;
+}
+
+export default defineComponent({
+  props: {
+    link: {
+      type: Object as PropType<INavItemProps>,
+      required: true,
+      default: (): INavItemProps => {
+        return {
+          label: "",
+          href: "",
+        };
+      },
+    },
+  },
+  setup(props) {
+    const isInternalLink = (): boolean => {
+      if (props.link.href.length && props.link.href.includes("http"))
+        return false;
+      return true;
+    };
+
+    return { isInternalLink };
+  },
+});
+</script>
+
+<style lang="scss">
+#nav-item {
+  &.nav-item {
+    padding-left: 1vw;
+    padding-right: 1vw;
+    a {
+      text-decoration: none;
+      color: #fff;
+      font-weight: normal;
+      display: inline-block;
+      position: relative;
+      &:hover {
+        font-weight: bold;
+      }
+      &:after {
+        content: "";
+        position: absolute;
+        width: 100%;
+        transform: scaleX(0);
+        height: 2px;
+        bottom: 0;
+        left: 0;
+        background-color: #fff;
+        transform-origin: bottom center;
+        transition: transform 0.15s ease-out;
+      }
+      &:hover:after {
+        transform: scaleX(1);
+        transform-origin: bottom center;
+      }
+      &.router-link-active {
+        font-weight: bolder;
+      }
+    }
+  }
+  .hover-underline-animation {
+    display: inline-block;
+    position: relative;
+    color: #0087ca;
+  }
+  .hover-underline-animation:after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    transform: scaleX(0);
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    background-color: #0087ca;
+    transform-origin: bottom center;
+    transition: transform 0.25s ease-out;
+  }
+  .hover-underline-animation:hover:after {
+    transform: scaleX(1);
+    transform-origin: bottom center;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
+import Router from "./router";
 
 import App from "./App.vue";
 
 const app = createApp(App);
-app.mount("#app");
+app.use(Router).mount("#app");
