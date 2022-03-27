d0482cb7b4cf5521090b8c50e3c1e15261ea79f0
Neal Cynkus <neal.cynkus@rewardstyle.com>
2022-03-27 11:29:52 -0700

    added async to home component suspense now shows

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,23 @@
   <div class="container-fluid" id="home-page">
     <div class="row">
       <div class="offset-md-3 col-12 col-md-6 text-center">
-        <p>Hello From Vue 3</p>
+        <p>Covid data {{ data }}</p>
       </div>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import axios from "axios";
+
+export default defineComponent({
+  async setup() {
+    const data = await axios.get(
+      "https://api.covidtracking.com/v1/states/current.json"
+    );
+
+    return { data };
+  },
+});
+</script>
